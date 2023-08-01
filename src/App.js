a2e124b56142bddb43887b0726741b322913c254
Yash Lad <yashlad727@gmail.com>
2023-08-01 13:20:43 +0530

    List rendering using map()

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import './App.css';
 // import ClassClick from './components/ClassClick';
 // import EventBind from './components/EventBind';
 // import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
+// import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 
 function App() {
   return (
     <div className="App">
-      <UserGreeting />
+      <NameList />
+
+      {/* <UserGreeting /> */}
 
       {/* <ParentComponent /> */}
 
---
+++ src/components/NameList.js
@@ -0,0 +1,45 @@
+// List Rendering 
+
+import React from 'react'
+
+function NameList() {
+    const names = ['Burce', 'Clark', 'Diana']
+
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+        }, 
+        {
+            id: 2,
+            name: 'Clark',
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Diana',
+            age: 28,
+            skill: 'Vue'
+        }
+    ]
+
+  return (
+    <div>
+        {/* Normal Array slicing */}
+        {/* <h2>{names[0]}</h2>
+        <h2>{names[1]}</h2>
+        <h2>{names[2]}</h2> */}
+
+        {/* Map method: */}
+        {
+            names.map(name => <h2>{name}</h2>)
+        }
+    </div>
+    
+  )
+}
+
+export default NameList
