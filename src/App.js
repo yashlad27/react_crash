900532442964e54014a47ce891d40c3d4de20135
Yash Lad <yashlad727@gmail.com>
2023-08-02 21:54:40 +0530

    css modules in JSX

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ import './App.css';
 // import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 function App() {
   return (
     <div className="App">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+
       <Inline />
 
       <Stylesheet primary={true}/>
