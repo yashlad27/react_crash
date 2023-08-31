a5a72b7a4a47b263654ac543fa9b7d29996e62c8
Yash Lad <yashlad727@gmail.com>
2023-08-31 16:37:33 +0530

    Use of React.Fragment

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ import './App.css';
 // import styles from './appStyles.module.css';
 // import Form from './components/Form';
 // import LifeCycleA from './components/LifeCycleA';
-import FragmentDemo from './components/FragmentDemo';
+// import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
-      <FragmentDemo />
+      <Table />
+
+      {/* <FragmentDemo /> */}
 
       {/* <LifeCycleA /> */}
 
--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -3,8 +3,8 @@ import React from 'react'
 function FragmentDemo() {
   return (
     <React.Fragment>
-    <h1>FragmentDemo</h1>
-    <p>This describes the Fragment Demo Component</p>
+        <h1>FragmentDemo</h1>
+        <p>This describes the Fragment Demo Component</p>
     </React.Fragment>
   )
 }
